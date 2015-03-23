f56d2fdd75e9a5308184c2fe61a0d1026c7d4dd6
Serdar Karatekin <karatekinserdar@gmail.com>
2015-03-23 00:23:57 -0600

    Set up action menu.

--- NewsReader/Macros.h
+++ NewsReader/Macros.h
@@ -10,3 +10,5 @@
 
 #define HEXCOLORA(rgbValue, a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0 green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0 blue:((float)(rgbValue & 0xFF)) / 255.0 alpha:a]
 #define HEXCOLOR(rgbValue) HEXCOLORA(rgbValue, 1.0)
+
+#define clamp(n,min,max)                        ((n < min) ? min : (n > max) ? max : n)
--- NewsReader/NewsViewController.m
+++ NewsReader/NewsViewController.m
@@ -7,11 +7,16 @@
 //
 
 #import "NewsViewController.h"
+#import "Macros.h"
 
 @interface NewsViewController ()
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
+
+@property (weak, nonatomic) IBOutlet UIView *leftAction;
+@property (weak, nonatomic) IBOutlet UIView *centerAction;
+@property (weak, nonatomic) IBOutlet UIView *rightAction;
 
 @end
 
@@ -23,6 +28,8 @@
     
     UIColor *patternColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"egg_shell"]];
     self.view.backgroundColor = patternColor;
+    
+//    [self configActions];
     
 }
 
@@ -49,6 +56,111 @@
     self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width * 10, self.scrollView.frame.size.height);
 }
 
+#pragma mark - Helper Methods
+
+// create the proper placement and perspective for the action menu segments
+- (void)configActions {
+    [self setAnchorPoint:CGPointMake(1.5, 0.5) forView:self.leftAction];
+    [self setAnchorPoint:CGPointMake(-0.5,0.5) forView:self.rightAction];
+}
+
+// Taken from: http://stackoverflow.com/a/5666430
+- (void)setAnchorPoint:(CGPoint)anchorPoint forView:(UIView *)view {
+    
+    CGPoint newPoint = CGPointMake(view.bounds.size.width * anchorPoint.x,
+                                   view.bounds.size.height * anchorPoint.y);
+    CGPoint oldPoint = CGPointMake(view.bounds.size.width * view.layer.anchorPoint.x,
+                                   view.bounds.size.height * view.layer.anchorPoint.y);
+    
+    newPoint = CGPointApplyAffineTransform(newPoint, view.transform);
+    oldPoint = CGPointApplyAffineTransform(oldPoint, view.transform);
+    
+    CGPoint position = view.layer.position;
+    
+    position.x -= oldPoint.x;
+    position.x += newPoint.x;
+    
+    position.y -= oldPoint.y;
+    position.y += newPoint.y;
+    
+    view.layer.position = position;
+    view.layer.anchorPoint = anchorPoint;
+}
+
+- (float)relativeOffset
+{
+    float offset = self.scrollView.contentOffset.x;
+    float contentSize = self.scrollView.contentSize.width;
+    float width = self.scrollView.frame.size.width;
+    
+    // don't process when bouncing beyond the range of the scroll view
+    // clamp offset value between first and last segment
+    offset = clamp(offset, 0., contentSize - width);
+    
+    // now divide into the number of segments
+    offset = fmodf(offset, width);
+    
+    return offset;
+}
+
+- (void)layoutActionMenuForOffset:(float)offset
+{
+    // we're splitting the offset into three equally-spaced animations
+    // Timeline:      |----|----|----|----|----|
+    // Left action:   |--------------|
+    // Center action:      |--------------|
+    // Right action:            |--------------|
+    
+    float maxOffset = self.scrollView.frame.size.width;
+    // marginalOffset is the width of a single animation
+    float marginalOffset = maxOffset / 5.0 * 3.0;
+    // iterationGap is the width between sequential animation starts
+    float iterationGap = maxOffset / 5;
+    
+    float leftActionOffset = clamp(offset, 0., marginalOffset);
+    float leftActionRotationAmount = leftActionOffset / marginalOffset;
+    leftActionRotationAmount = [self transformRotation:leftActionRotationAmount];
+    [self rotateAction:self.leftAction byAmount:leftActionRotationAmount];
+    
+    float centerActionOffset = clamp(offset, iterationGap, marginalOffset + iterationGap);
+    centerActionOffset = centerActionOffset - iterationGap;
+    float centerActionRotationAmount = centerActionOffset / marginalOffset;
+    centerActionRotationAmount = [self transformRotation:centerActionRotationAmount];
+    [self rotateAction:self.centerAction byAmount:centerActionRotationAmount];
+    
+    float rightActionOffset = clamp(offset, iterationGap * 2, maxOffset);
+    rightActionOffset = rightActionOffset - (iterationGap * 2);
+    float rightActionRotationAmount = rightActionOffset / marginalOffset;
+    rightActionRotationAmount = [self transformRotation:rightActionRotationAmount];
+    [self rotateAction:self.rightAction byAmount:rightActionRotationAmount];
+}
+
+- (float)transformRotation:(float)rotationAmount
+{
+    // map our rotation so it flips back down at the center point and the layer is never upside down
+    float returnVal = rotationAmount >= 0.5 ? rotationAmount - 1 : rotationAmount;
+    
+    // because the layer has no height, it'll disappear for a moment at this value if we don't fix it
+    returnVal = returnVal == -0.5 ? -0.4999 : returnVal;
+    
+    // mapping rotation to a true circular path
+    returnVal = asinf(2 * returnVal) / M_PI * 2;
+    
+    return returnVal;
+}
+
+- (void)rotateAction:(UIView *)action byAmount:(float)amount
+{
+    CATransform3D rotationTransform = CATransform3DIdentity;
+    
+    rotationTransform.m33 = 0.0;
+    rotationTransform.m34 = 0.005;
+    rotationTransform = CATransform3DRotate(rotationTransform, amount * M_PI / 2, 1.0, 0.0, 0.0);
+    
+    CALayer *layer = action.layer;
+    layer.transform = rotationTransform;
+}
+
 #pragma mark - UIScrollViewDelegate methods
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
@@ -56,6 +168,8 @@
     for (UIView *newsTile in [scrollView subviews]) {
         [newsTile setNeedsLayout];
     }
+    
+    [self layoutActionMenuForOffset:[self relativeOffset]];
 }
 
 #pragma mark - NewsTileDelegate methods
