4e1c896998923059966416472491efb5acdf58a4
Serdar Karatekin <karatekinserdar@gmail.com>
2015-03-27 03:20:14 -0600

    Remove unnecessary comment

--- NewsReader/NewsViewController.m
+++ NewsReader/NewsViewController.m
@@ -33,7 +33,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
     [self setupView];
     [self loadNewsWithQuery:@"France"];
