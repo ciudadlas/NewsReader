5d5678e20b047f1b345d6a2a68691d71b12c1514
Serdar Karatekin <karatekinserdar@gmail.com>
2015-03-27 15:03:03 -0600

    Update copyright info

--- NewsReader/NSDateFormatter+ThreadSafe.h
+++ NewsReader/NSDateFormatter+ThreadSafe.h
@@ -2,8 +2,8 @@
 //  NSDateFormatter+ThreadSafe.h
 //  Skate
 //
-//  Created by Serdar Karatekin on 12/16/13.
-//  Copyright (c) 2013 Crispin Porter + Bogusky. All rights reserved.
+//  Created by Serdar Karatekin on 3/27/15.
+//  Copyright (c) 2015 Serdar Karatekin. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- NewsReader/NSDateFormatter+ThreadSafe.m
+++ NewsReader/NSDateFormatter+ThreadSafe.m
@@ -2,8 +2,8 @@
 //  NSDateFormatter+ThreadSafe.m
 //  Skate
 //
-//  Created by Serdar Karatekin on 12/16/13.
-//  Copyright (c) 2013 Crispin Porter + Bogusky. All rights reserved.
+//  Created by Serdar Karatekin on 3/27/15.
+//  Copyright (c) 2015 Serdar Karatekin. All rights reserved.
 //
 
 #import "NSDateFormatter+ThreadSafe.h"
